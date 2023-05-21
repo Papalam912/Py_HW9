f63ee157559a8cb80a4faebdfc1858f12277c8ad
e.polovinkin <polkill@list.ru>
2023-05-21 15:23:14 +0300

    commit

---
+++ Py_HW9.py
@@ -0,0 +1,10 @@
+# 9.1[40]: Работа в Google Colab. Файл california_housing_train.csv, который находится в папке sample_data
+# Для домов, где кол-во людей от 0 до 500 (population) вывести информацию  о цене  дома(median_house_value):
+
+# - максимальное значение
+# - минимальное значение
+# - среднее
+# - медиану
+
+# #### 9.2[42]: Работа в Google Colab. Файл california_housing_train.csv, который находится в папке sample_data
+# Узнать какая максимальная households в зоне минимального значения population
